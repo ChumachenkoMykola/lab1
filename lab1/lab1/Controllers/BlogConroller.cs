605c35d58dd861014182f97574aebeb109f52789
ChumachenkoMykola <mykola.chumachenko@microsoft.wsei.edu.pl>
2025-10-04 09:35:04 +0200

    Blog Added

---
+++ lab1/lab1/Controllers/BlogConroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab1.Controllers
+{
+    public class BlogConroller : Controller
+    {
+        private readonly ILogger<BlogConroller> _logger;
+
+        public BlogConroller(ILogger<BlogConroller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new BlogViewModel()
+            {
+                Title = "My first blog post",
+                Description = "This is the description of my first blog post."
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
